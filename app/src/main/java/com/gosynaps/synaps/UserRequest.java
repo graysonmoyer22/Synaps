9008067ecc70303aad327afc88237f591be94ce8
Will <wfoy@andrew.cmu.edu>
2018-09-22 17:22:39 -0400

    ran somehow

--- app/src/main/java/com/gosynaps/synaps/UserRequest.java
+++ app/src/main/java/com/gosynaps/synaps/UserRequest.java
@@ -1,7 +1,10 @@
 package com.gosynaps.synaps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserRequest extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class UserRequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_request);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserRequest.this,Pop.class));
+            }
+        });
     }
 }
