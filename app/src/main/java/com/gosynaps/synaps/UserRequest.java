e0d0f7fcefe793619c17856369bbe772af65eb19
Will <wfoy@andrew.cmu.edu>
2018-09-22 18:55:17 -0400

    done

--- app/src/main/java/com/gosynaps/synaps/UserRequest.java
+++ app/src/main/java/com/gosynaps/synaps/UserRequest.java
@@ -1,24 +1,32 @@
 package com.gosynaps.synaps;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class UserRequest extends AppCompatActivity {
-
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_request);
+        myDialog = new Dialog(this);
+    }
 
-        Button b = (Button) findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
+    public void ShowPopup(View v) {
+        TextView txtclose;
+        myDialog.setContentView(R.layout.popwindow);
+        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(UserRequest.this,Pop.class));
+                myDialog.dismiss();
             }
         });
+        myDialog.show();
     }
 }
